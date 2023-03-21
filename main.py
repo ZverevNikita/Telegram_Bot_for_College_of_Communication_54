9ad73e14e8d67d1be20eaa614a66f031874d11c9
zv_nik <105583773+ZverevNikita@users.noreply.github.com>
2023-03-21 22:12:56 +0300

    Телеграм Бот для КС54

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from weather import get_weather_string
 token = '6201705760:AAE_kjoIWWkC9VWnOoqnDHeS1QqNQiu7Rb8'
 bot = telebot.TeleBot(token)
 
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
@@ -15,9 +14,7 @@ def start_message(message):
     button4 = types.KeyboardButton(text="Спортивные залы")
     button5 = types.KeyboardButton(text="Погода", request_location=True)
     markup.add(button1, button2, button3, button4, button5)
-    bot.send_message(message.chat.id, "Приветствую Вас на странице нашего бота «Спортивная жизнь в КС54!»",
-                     reply_markup=markup)
-
+    bot.send_message(message.chat.id, "Приветствую Вас на странице нашего бота «Спортивная жизнь в КС54!»", reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def function(message):
